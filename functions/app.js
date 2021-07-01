8e78c760a3191168a693ab3e3795335e6a760302
Óscar Llorente <llorente_oscar@hotmail.com>
2021-06-30 22:05:48 -0500

    Agregando archivos

---
+++ functions/app.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const serverless = require('serverless-http')
+const mongoose = require('mongoose')
+const { error404, errorHandler } = require('./middlewares')
+
+const app = express()
+
+mongoose.connect(process.env.MONGODB || 'mongodb://localhost:27017/eCommerceDev', {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log(`>>> [DB] is connected... <<<`))
+    .catch(error => console.log(`<<< [ERROR]: ${error} >>>`))
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(cookieParser())
+
+app.use('/.netlify/functions/app', require('./routes'))
+
+// catch 404 and forward to error handler
+app.use(error404)
+// error handler
+app.use(errorHandler)
+
+module.exports.handler = serverless(app)
